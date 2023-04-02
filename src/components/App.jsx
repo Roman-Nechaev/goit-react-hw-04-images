512425fd20cafd57d4914607c4fc39e55348b658
Roman-Nechaev <nrchaevroman@gmail.com>
2023-04-02 23:26:35 +0300

    hook App fetchData

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
 import { fetchImage } from './Api/api';
@@ -14,122 +14,101 @@ import { ErrorData } from './Error/ErrorData/ErrorData';
 
 import { Container } from './App.styled';
 
-export class App extends Component {
-  state = {
-    images: null,
-    pageNum: 2,
-    search: '',
-    isLoading: false,
-    showModal: false,
-    modalImg: null,
-    btnVision: true,
-    error: null,
-    isLoadingSpinner: false,
-  };
+export function App() {
+  const [search, setSearch] = useState('');
+  const [images, setImages] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImg, setModalImg] = useState(null);
+  const [btnVision, setBtnVision] = useState(true);
+  const [isLoadingSpinner, setIsLoadingSpinner] = useState(false);
 
-  acceptSearch = async search => {
-    if (this.state.search === search || search === '') {
+  useEffect(() => {
+    if (!search) {
       return;
     }
 
-    try {
-      this.setState({ images: null });
-      this.setState({ isLoading: true });
-      this.setState({ search: search });
-      this.setState({ pageNum: 2 });
-      this.setState({ btnVision: true });
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        setIsLoadingSpinner(true);
+        setBtnVision(true);
+        const response = await fetchImage(search, pageNum);
 
-      const response = await fetchImage(search);
-      this.setState({ images: response.hits });
+        setImages(prevImage => [...prevImage, ...response.hits]);
 
-      if (response.total === 0) {
-        this.setState({ error: errorMassage(search) });
+        if (response.total === 0) {
+          setError(errorMassage(search));
+        }
+        if (response.hits.length < 1) {
+          setBtnVision(false);
+          Report.info(
+            "That's all",
+            "We're sorry, but you've reached the end of search results.",
+            'Okay'
+          );
+          return;
+        }
+      } catch {
+        setError(ErrorData());
+      } finally {
+        setIsLoading(false);
+        setIsLoadingSpinner(false);
       }
-    } catch {
-      this.setState({ error: ErrorData() });
-    } finally {
-      this.setState({ isLoading: false });
+    };
+
+    fetchData();
+  }, [pageNum, search]);
+
+  const acceptSearch = queryImg => {
+    if (queryImg === search) {
+      return;
     }
+    setImages([]);
+    setPageNum(1);
+    setSearch(queryImg);
   };
 
-  onClickPageUp = async () => {
-    try {
-      this.setState({ isLoadingSpinner: true });
-      const { pageNum, search } = this.state;
-      this.setState(prevState => {
-        return { pageNum: prevState.pageNum + 1 };
-      });
-      const response = await fetchImage(search, pageNum);
-
-      const nextPictures = response.hits;
-      if (nextPictures.length < 1) {
-        this.setState({ btnVision: false });
-        Report.info(
-          "That's all",
-          "We're sorry, but you've reached the end of search results.",
-          'Okay'
-        );
-        return;
-      }
-      this.setState(prevState => ({
-        images: [...prevState.images, ...nextPictures],
-      }));
-    } catch {
-      this.setState({ error: ErrorData() });
-    } finally {
-      this.setState({ isLoading: false });
-      this.setState({ isLoadingSpinner: false });
-    }
+  const onClickPageUp = () => {
+    setPageNum(pageNum + 1);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(prevShowModal => (prevShowModal = !showModal));
   };
 
-  updateModalPicture = img => {
-    this.setState({ modalImg: img });
+  const updateModalPicture = img => {
+    setModalImg(img);
   };
 
-  render() {
-    const {
-      images,
-      isLoading,
-      showModal,
-      modalImg,
-      error,
-      btnVision,
-      isLoadingSpinner,
-    } = this.state;
+  return (
+    <>
+      <Container>
+        <Searchbar onSubmit={acceptSearch} />
+        {isLoading && <Loader />}
 
-    return (
-      <>
-        <Container>
-          <Searchbar onSubmit={this.acceptSearch} />
-          {isLoading && <Loader />}
+        {error && error}
 
-          {error && error}
-
-          {images && (
-            <ImageGallery
-              images={images}
-              onClick={this.toggleModal}
-              onUpdateModalPicture={this.updateModalPicture}
+        {images && (
+          <ImageGallery
+            images={images}
+            onClick={toggleModal}
+            onUpdateModalPicture={updateModalPicture}
+          />
+        )}
+        {images && images.length > 0 && btnVision && (
+          <>
+            <LoadMoreBtn
+              onLoadMore={onClickPageUp}
+              isLoadingSpin={isLoadingSpinner}
             />
-          )}
-          {images && images.length > 0 && btnVision && (
-            <>
-              <LoadMoreBtn
-                onLoadMore={this.onClickPageUp}
-                isLoadingSpin={isLoadingSpinner}
-              />
-            </>
-          )}
+          </>
+        )}
 
-          {showModal && (
-            <Modal onClose={this.toggleModal} onGiveImg={modalImg} />
-          )}
-        </Container>
-      </>
-    );
-  }
+        {showModal && <Modal onClose={toggleModal} onGiveImg={modalImg} />}
+      </Container>
+    </>
+  );
 }
